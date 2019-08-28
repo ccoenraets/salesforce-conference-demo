7d338bc2a41fe9dbed82c9aa7b3f5ce548d7ffca
SYMC\Swapnil_Kandekar <swapnil_kandekar@symantec.com>
2019-08-28 21:37:59 +0530

    Commiting to Develop

--- client/js/app.js
+++ client/js/app.js
@@ -59,7 +59,7 @@ function showSessionDetails(sessionId) {
                                 session.Speaker__r.First_Name__c +
                             '</li>' +
                             '<li class="table-view-cell">' +
-                                ('Description Chi Value:'+session.Session__r.Description__c || 'No description yet') +
+                                ('Description in Develop:'+session.Session__r.Description__c || 'No description yet') +
                             '</li>' +
                         '</ul>' +
                     '</div>' +
